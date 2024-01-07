81313b1b52549ae1518f71d3fda83a399e0abc60
Posdadckad <155913193+Posdadckad@users.noreply.github.com>
2024-01-08 00:15:49 +0500

    Update Crack.bat

--- Crack.bat
+++ Crack.bat
@@ -1,6 +1,6 @@
 curl -o C:\WILD\fixV6.bat https://raw.githubusercontent.com/Posdadckad/Wild-crack/main/fixV6.bat
 @echo off
-echo NoEscape loadi
+echo NoEscape loading..
 timeout /nobreak /t 3 >nul
 echo Poshel naxuy
 timeout /nobreak /t 1 >nul
